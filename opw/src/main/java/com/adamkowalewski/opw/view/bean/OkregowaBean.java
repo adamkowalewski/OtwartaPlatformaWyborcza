cdc7eb83fd846e7235a1470db3a9cac5bae3cbb4
pwl <pwl@pwl.pl>
2015-04-05 14:37:23 +0200

    Project structure improvements
    -changes package com.adamkowalewski.opw.session to com.adamkowalewski.opw.view
    -moved package com.adamkowalewski.opw.gen.controller.util to com.adamkowalewski.opw.view.controller.util
    -added to .gitignore patterns for IntelliJ IDEA IDE

---
+++ opw/src/main/java/com/adamkowalewski/opw/view/bean/OkregowaBean.java
@@ -0,0 +1,65 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2015 Adam Kowalewski.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.adamkowalewski.opw.view.bean;
+
+import com.adamkowalewski.opw.entity.OpwOkregowaKomisja;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+/**
+ * Provides access to Komisja Okregowa.
+ *
+ * @author Adam Kowalewski
+ */
+@Stateless
+public class OkregowaBean extends AbstractOpwFacade<OpwOkregowaKomisja> {
+
+    @PersistenceContext(unitName = PU_OPW)
+    private EntityManager em;
+
+    public OkregowaBean() {
+        super(OpwOkregowaKomisja.class);
+    }
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public OpwOkregowaKomisja findOkregowa(String pkwId) {
+        Query q = em.createNamedQuery("OpwOkregowaKomisja.findByPkwId");
+        q.setParameter("pkwId", pkwId);
+        return (OpwOkregowaKomisja) q.getSingleResult();
+    }
+
+    public List<OpwOkregowaKomisja> findOkregowaLike(String pkwId) {
+        Query q = em.createQuery("SELECT o FROM OpwOkregowaKomisja o WHERE o.pkwId like :pkwId");
+        q.setParameter("pkwId", pkwId);
+        return q.getResultList();
+    }
+
+}
---
+++ opw/src/main/java/com/adamkowalewski/opw/view/controller/ConfigController.java
@@ -0,0 +1,81 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2015 Adam Kowalewski.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.adamkowalewski.opw.view.controller;
+
+import com.adamkowalewski.opw.view.dto.ConfigMailDto;
+import java.io.Serializable;
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+/**
+ * Unified configuration. TODO load from database TODO load from properties
+ *
+ * @author Adam Kowalewski
+ */
+@Named
+@SessionScoped
+public class ConfigController implements Serializable {
+
+    /**
+     * Application salt may not be changed and is therefore hardcoded.
+     */
+    private final String APP_SALT = "abc";
+
+    private ConfigMailDto configMail;
+
+    private boolean configMailOutboundActive;
+
+    public ConfigController() {
+
+    }
+
+    @PostConstruct
+    public void initConfigController() {
+        configMail = new ConfigMailDto("Otwarta Platforma Wyborcza", "opw@adamkowalewski.com", "http://91.250.114.134:8080/opw");
+        // ToDo enable when JAAS active 
+        configMailOutboundActive = false;
+    }
+
+    public String getAPP_SALT() {
+        return APP_SALT;
+    }
+
+    public ConfigMailDto getConfigMail() {
+        return configMail;
+    }
+
+    public void setConfigMail(ConfigMailDto configMail) {
+        this.configMail = configMail;
+    }
+
+    public boolean isConfigMailOutboundActive() {
+        return configMailOutboundActive;
+    }
+
+    public void setConfigMailOutboundActive(boolean configMailOutboundActive) {
+        this.configMailOutboundActive = configMailOutboundActive;
+    }
+
+}
